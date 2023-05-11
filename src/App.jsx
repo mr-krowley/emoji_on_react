42f0067b3f92c535d355c57ec8e55b774568b26c
Viktor <midyukinv@mail.ru>
2023-05-11 17:47:59 +0300

    add App logik

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 
 import { useEffect, useState } from 'react';
-
-import { Search } from './components/Search/Search';
 import { Main } from './components/Main/Main';
 import { Header } from './components/Header/Header';
 import { Footer } from './components/Footer/Footer';
@@ -33,16 +31,24 @@ function App() {
   const [value, setValue] = useState(""); // состояние для поиска
   //функция возращает что записано в Search
   const searchCard = (evt) => {
-    setValue(evt.target.value);
+    setValue(evt.target.value)
   };
-  // отфильтрованные карточки по поиску записываем в новвую переменную
-  const filterData = data.filter((elem) => elem.title.includes(value));
+  const newValue = value.trim().toLowerCase();
+  
 
-  console.log(data);
-  console.log(value);
-  console.log(filterData);
+
+  
+  // отфильтрованные карточки по поиску записываем в новвую переменную
+  const filterData = data.filter(
+    (elem) =>
+      elem.title.trim().toLowerCase().includes(newValue) ||
+      elem.keywords.trim().toLowerCase().includes(newValue)
+  );
+  
  
   
+
+
   return (
     <>
       <Header value={value} inputHandler={searchCard} />
